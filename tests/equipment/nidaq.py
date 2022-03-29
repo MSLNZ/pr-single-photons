4f813df47cb34f35ce69e2f3d463b26898401370
jborbely <joe.borbely@gmail.com>
2022-03-29 14:29:15 +1300

    update nidaq.py for recent changes to the NIDAQ class

--- tests/equipment/nidaq.py
+++ tests/equipment/nidaq.py
@@ -2,6 +2,7 @@
 Test that photons/equipment/nidaq.py is working properly.
 """
 import math
+from time import perf_counter
 
 import numpy as np
 
@@ -49,20 +50,49 @@ assert daq.digital_in(port_str, '0:7') == [False] * 8
 #
 # Analog Input
 #
-assert daq.analog_in(0).shape == (1,)
-assert daq.analog_in(0, n=5).shape == (5,)
-assert daq.analog_in('0:3').shape == (4, 1)
-assert daq.analog_in('0:7', n=25).shape == (8, 25)
+data, dt = daq.analog_in(0)
+assert data.shape == (1,)
+assert dt == 0.001
+
+data, dt = daq.analog_in(0, nsamples=2, rate=10)
+assert data.shape == (2,)
+assert dt == 0.1
+
+# only 1 sample is acquired, so the `rate` keyword argument is ignored
+data, dt = daq.analog_in('0:3', rate=1)
+assert data.shape == (4, 1)
+assert dt == 0.001
+
+data, dt = daq.analog_in('0:3', nsamples=2, rate=100e3)
+assert data.shape == (4, 2)
+assert dt == 1e-5
+
+data, dt = daq.analog_in('0:7', duration=1, rate=100)
+assert data.shape == (8, 100)
+assert dt == 0.01
+x = np.asarray([i*dt for i in range(100)])
+x2 = daq.time_array(dt, 100)
+assert np.array_equal(x, x2)
 
 #
 # Analog Output
 #
 assert daq.analog_out(0, 0) == 1
-assert daq.analog_out(0, 1.2) == 1
-assert daq.analog_out(0, [-0.5]) == 1
-assert daq.analog_out(0, [-2, -1, 0, 1, 2]) == 5
+assert daq.analog_out(0, 0.1) == 1
+assert daq.analog_out(0, [-0.1]) == 1
+assert daq.analog_out(0, [-0.2, -0.1, 0., 0.1, 0.2]) == 5
 assert daq.analog_out('0:1', np.array([0.1, -0.1])) == 1
 assert daq.analog_out('0:1', [0., 0.]) == 1
+assert daq.analog_out('0:1', [[-0.2, -0.1, 0., 0.1, 0.2], [0.2, 0.1, 0., -0.1, -0.2]]) == 5
+
+t0 = perf_counter()
+assert daq.analog_out('0', [0., 0.1, 0.2], rate=1) == 3
+assert perf_counter() - t0 > 2.0
+
+values = np.sin(np.linspace(0, 2*np.pi, 1000))
+assert daq.analog_out(0, values, rate=1e5) == 1000
+
+assert daq.analog_out('0:1', [[0.], [0.]]) == 1
 
 #
 # Count edges
