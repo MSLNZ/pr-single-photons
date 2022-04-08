6c4bcd9fdc560d466b4d130e29964958eb71f6dc
jborbely <joe.borbely@gmail.com>
2022-04-08 15:33:17 +1200

    only import the qt package

--- photons/gui/equipment/dmm.py
+++ photons/gui/equipment/dmm.py
@@ -1,20 +1,8 @@
 """
 Widget for a digital multimeter.
 """
+from msl import qt
 from msl.qt.convert import number_to_si
-from msl.qt import (
-    QtWidgets,
-    QtCore,
-    QtGui,
-    Qt,
-    SpinBox,
-    DoubleSpinBox,
-    Button,
-    Worker,
-    Thread,
-    prompt,
-    application,
-)
 
 from . import (
     BaseWidget,
@@ -22,7 +10,7 @@ from . import (
 )
 
 
-class FetchWorker(Worker):
+class FetchWorker(qt.Worker):
 
     def __init__(self, connection, trigger_mode):
         """Calls :meth:`photons.equipment.dmm.DMM.fetch`"""
@@ -55,49 +43,50 @@ class DMM(BaseWidget):
         self.info = {}  # gets updated in update_tooltip()
         self.unit_map = {'CURRENT': 'A', 'VOLTAGE': 'V', 'DCI': 'A', 'DCV': 'V'}
 
-        self.value_lineedit = QtWidgets.QLineEdit()
+        self.value_lineedit = qt.QtWidgets.QLineEdit()
         self.value_lineedit.setReadOnly(True)
-        self.value_lineedit.setSizePolicy(QtWidgets.QSizePolicy.Expanding, QtWidgets.QSizePolicy.Expanding)
-        self.value_lineedit.setAlignment(Qt.AlignRight)
+        self.value_lineedit.setSizePolicy(qt.QtWidgets.QSizePolicy.Expanding, qt.QtWidgets.QSizePolicy.Expanding)
+        self.value_lineedit.setAlignment(qt.Qt.AlignRight)
         self.update_tooltip()
 
-        self.uncertainty_lineedit = QtWidgets.QLineEdit()
+        self.uncertainty_lineedit = qt.QtWidgets.QLineEdit()
         self.uncertainty_lineedit.setReadOnly(True)
-        self.uncertainty_lineedit.setSizePolicy(QtWidgets.QSizePolicy.Expanding, QtWidgets.QSizePolicy.Expanding)
-        self.uncertainty_lineedit.setAlignment(Qt.AlignRight)
+        self.uncertainty_lineedit.setSizePolicy(qt.QtWidgets.QSizePolicy.Expanding, qt.QtWidgets.QSizePolicy.Expanding)
+        self.uncertainty_lineedit.setAlignment(qt.Qt.AlignRight)
         self.uncertainty_lineedit.setToolTip('The standard deviation')
 
-        self.config_button = Button(
+        self.config_button = qt.Button(
             icon='shell32|239',
             left_click=self.on_edit_configuration,
             tooltip='Edit the configuration'
         )
 
-        self.font = QtGui.QFont(self.value_lineedit.font())
-        self.font_size_spinbox = SpinBox()
+        self.font = qt.QtGui.QFont(self.value_lineedit.font())
+        self.font_size_spinbox = qt.SpinBox()
         self.font_size_spinbox.setRange(0, 999)
         self.font_size_spinbox.setToolTip('The font size to use to display the value')
         self.font_size_spinbox.valueChanged.connect(self.on_font_size_changed)
 
-        self.live_spinbox = SpinBox()
+        self.live_spinbox = qt.SpinBox()
         self.live_spinbox.setRange(0, 99999)
         self.live_spinbox.setValue(1000)
         self.live_spinbox.setToolTip('The number of milliseconds to wait between live updates')
         self.live_spinbox.valueChanged.connect(self.on_live_spinbox_changed)
 
-        self.live_checkbox = QtWidgets.QCheckBox()
+        self.live_checkbox = qt.QtWidgets.QCheckBox()
         self.live_checkbox.setChecked(True)
         self.live_checkbox.setToolTip('Enable live update?')
         self.live_checkbox.clicked.connect(self.on_live_checkbox_changed)
 
-        hbox = QtWidgets.QHBoxLayout()
+        hbox = qt.QtWidgets.QHBoxLayout()
         hbox.addWidget(self.config_button)
-        hbox.addSpacerItem(QtWidgets.QSpacerItem(1, 1, QtWidgets.QSizePolicy.Expanding, QtWidgets.QSizePolicy.Minimum))
+        hbox.addSpacerItem(qt.QtWidgets.QSpacerItem(
+            1, 1, qt.QtWidgets.QSizePolicy.Expanding, qt.QtWidgets.QSizePolicy.Minimum))
         # hbox.addWidget(self.font_size_spinbox)
         hbox.addWidget(self.live_spinbox)
         hbox.addWidget(self.live_checkbox)
 
-        vbox = QtWidgets.QVBoxLayout()
+        vbox = qt.QtWidgets.QVBoxLayout()
         vbox.addLayout(hbox)
         vbox.addWidget(self.value_lineedit)
         vbox.addWidget(self.uncertainty_lineedit)
@@ -107,9 +96,9 @@ class DMM(BaseWidget):
             connection.fetched.connect(self.update_lineedits)
             connection.config_changed.connect(self.on_config_changed)
 
-        self.thread = Thread(FetchWorker)
+        self.thread = qt.Thread(FetchWorker)
 
-        self.timer = QtCore.QTimer()
+        self.timer = qt.QtCore.QTimer()
         self.timer.timeout.connect(self.restart_thread)
         self.timer.start(self.live_spinbox.value())
 
@@ -194,7 +183,7 @@ class DMM(BaseWidget):
         scaled, prefix = number_to_si(average)
         self.value_lineedit.setText(f'{scaled:.3f} {prefix}{unit}')
         scaled, prefix = number_to_si(stdev)
-        self.uncertainty_lineedit.setText(f'\u00b1 {abs(scaled):.1f} {prefix}{unit}')
+        self.uncertainty_lineedit.setText(f'{qt.PLUS_MINUS} {abs(scaled):.1f} {prefix}{unit}')
 
     def restart_thread(self):
         """Slot for the QTimer.timeout signal."""
@@ -214,7 +203,7 @@ class DMM(BaseWidget):
         self.timer.start(self.live_spinbox.value())
 
 
-class ConfigureDialog(QtWidgets.QDialog):
+class ConfigureDialog(qt.QtWidgets.QDialog):
 
     def __init__(self, parent):
         """Edit the configuration of the DMM.
@@ -224,7 +213,7 @@ class ConfigureDialog(QtWidgets.QDialog):
         parent : :class:`.DMM`
             The parent widget.
         """
-        super(ConfigureDialog, self).__init__(parent, Qt.WindowCloseButtonHint)
+        super(ConfigureDialog, self).__init__(parent, qt.Qt.WindowCloseButtonHint)
 
         self.ask_user = False
         self.info = parent.info.copy()
@@ -248,38 +237,38 @@ class ConfigureDialog(QtWidgets.QDialog):
             self.EDGES = parent.connection.EDGES
             self.RANGES = parent.connection.RANGES
 
-        function_group = QtWidgets.QGroupBox('Function Settings')
+        function_group = qt.QtWidgets.QGroupBox('Function Settings')
 
-        self.function_combobox = QtWidgets.QComboBox()
+        self.function_combobox = qt.QtWidgets.QComboBox()
         self.function_combobox.addItems(sorted(set(v for v in self.FUNCTIONS.values())))
         self.function_combobox.setCurrentText(self.FUNCTIONS[self.info['function']])
         self.function_combobox.currentTextChanged.connect(self.on_function_changed)
         self.function_combobox.setToolTip('The function to measure')
 
-        self.range_combobox = QtWidgets.QComboBox()
+        self.range_combobox = qt.QtWidgets.QComboBox()
         self.update_range_combobox()
         self.range_combobox.currentTextChanged.connect(self.set_check_apply)
         self.range_combobox.setToolTip('The function range (not all multimeter\'s will support these ranges)')
 
-        self.nplc_combobox = QtWidgets.QComboBox()
+        self.nplc_combobox = qt.QtWidgets.QComboBox()
         self.nplc_combobox.addItems([str(v) for v in self.NPLCS.values()])
         self.nplc_combobox.setCurrentText(str(self.NPLCS[self.info['nplc']]))
         self.nplc_combobox.currentTextChanged.connect(self.set_check_apply)
         self.nplc_combobox.setToolTip('The number of power line cycles to integrate over')
 
-        self.nsamples_spinbox = SpinBox()
+        self.nsamples_spinbox = qt.SpinBox()
         self.nsamples_spinbox.setRange(1, int(1e6))
         self.nsamples_spinbox.setValue(self.info['nsamples'])
         self.nsamples_spinbox.valueChanged.connect(self.set_check_apply)
         self.nsamples_spinbox.setToolTip('The number of samples to acquire for each trigger')
 
-        self.auto_zero_combobox = QtWidgets.QComboBox()
+        self.auto_zero_combobox = qt.QtWidgets.QComboBox()
         self.auto_zero_combobox.addItems(sorted(set(self.AUTO_ZEROS.values())))
         self.auto_zero_combobox.setCurrentText(self.info['auto_zero'])
         self.auto_zero_combobox.currentTextChanged.connect(self.set_check_apply)
         self.auto_zero_combobox.setToolTip('The auto-zero mode')
 
-        form = QtWidgets.QFormLayout()
+        form = qt.QtWidgets.QFormLayout()
         form.addRow('Function:', self.function_combobox)
         form.addRow('Range:', self.range_combobox)
         form.addRow('NPLC:', self.nplc_combobox)
@@ -287,41 +276,41 @@ class ConfigureDialog(QtWidgets.QDialog):
         form.addRow('Auto Zero:', self.auto_zero_combobox)
         function_group.setLayout(form)
 
-        trigger_group = QtWidgets.QGroupBox('Trigger Settings')
+        trigger_group = qt.QtWidgets.QGroupBox('Trigger Settings')
 
-        self.mode_combobox = QtWidgets.QComboBox()
+        self.mode_combobox = qt.QtWidgets.QComboBox()
         self.mode_combobox.addItems(sorted(set(v for v in self.TRIGGERS.values())))
         self.mode_combobox.setCurrentText(self.TRIGGERS[self.info['trigger_mode']])
         self.mode_combobox.currentTextChanged.connect(self.set_check_apply)
         self.mode_combobox.setToolTip('The trigger mode')
 
-        self.edge_combobox = QtWidgets.QComboBox()
+        self.edge_combobox = qt.QtWidgets.QComboBox()
         self.edge_combobox.addItems(sorted(set(k for k in self.EDGES.keys())))
         self.edge_combobox.setCurrentText(self.EDGES[self.info['trigger_edge']])
         self.edge_combobox.currentTextChanged.connect(self.set_check_apply)
         self.edge_combobox.setToolTip('The edge to trigger on')
 
-        self.count_spinbox = SpinBox()
+        self.count_spinbox = qt.SpinBox()
         self.count_spinbox.setRange(1, 1e9)
         self.count_spinbox.setValue(self.info['trigger_count'])
         self.count_spinbox.valueChanged.connect(self.set_check_apply)
         self.count_spinbox.setToolTip('The number of triggers that are accepted by the '
                                       'instrument before returning to the "idle" trigger state')
 
-        self.auto_delay_checkbox = QtWidgets.QCheckBox()
+        self.auto_delay_checkbox = qt.QtWidgets.QCheckBox()
         self.auto_delay_checkbox.setCheckable(True)
         self.auto_delay_checkbox.setChecked(self.info['trigger_delay_auto'])
         self.auto_delay_checkbox.clicked.connect(self.auto_delay_changed)
         self.auto_delay_checkbox.setToolTip('Enable the auto-delay feature')
 
-        self.delay_spinbox = DoubleSpinBox()
+        self.delay_spinbox = qt.DoubleSpinBox()
         self.delay_spinbox.setRange(0, 3600)
         self.delay_spinbox.setValue(self.info['trigger_delay'])
         self.delay_spinbox.setDecimals(6)
         self.delay_spinbox.setEnabled(not self.auto_delay_checkbox.isChecked())
         self.delay_spinbox.setToolTip('The delay, in seconds, between the trigger signal and the first measurement')
 
-        form = QtWidgets.QFormLayout()
+        form = qt.QtWidgets.QFormLayout()
         form.addRow('Mode:', self.mode_combobox)
         form.addRow('Edge:', self.edge_combobox)
         form.addRow('# Triggers:', self.count_spinbox)
@@ -329,17 +318,25 @@ class ConfigureDialog(QtWidgets.QDialog):
         form.addRow('Delay:', self.delay_spinbox)
         trigger_group.setLayout(form)
 
-        self.reset_button = Button(icon=QtWidgets.QStyle.SP_DialogResetButton,
-                                   left_click=self.on_reset, tooltip='Reset [*RST]')
+        self.reset_button = qt.Button(
+            icon=qt.QtWidgets.QStyle.SP_DialogResetButton,
+            left_click=self.on_reset,
+            tooltip='Reset [*RST]')
 
-        self.clear_button = Button(icon=QtWidgets.QStyle.SP_DialogDiscardButton,
-                                   left_click=self.connection.clear, tooltip='Clear [*CLS]')
+        self.clear_button = qt.Button(
+            icon=qt.QtWidgets.QStyle.SP_DialogDiscardButton,
+            left_click=self.connection.clear,
+            tooltip='Clear [*CLS]')
 
-        self.apply_button = Button(icon=QtWidgets.QStyle.SP_DialogApplyButton,
-                                   left_click=self.on_apply_clicked, tooltip='Apply')
+        self.apply_button = qt.Button(
+            icon=qt.QtWidgets.QStyle.SP_DialogApplyButton,
+            left_click=self.on_apply_clicked,
+            tooltip='Apply')
 
-        self.cancel_button = Button(icon=QtWidgets.QStyle.SP_DialogCancelButton,
-                                    left_click=self.close, tooltip='Cancel')
+        self.cancel_button = qt.Button(
+            icon=qt.QtWidgets.QStyle.SP_DialogCancelButton,
+            left_click=self.close,
+            tooltip='Cancel')
 
         self.original_function = self.function_combobox.currentText()
         self.original_range = self.range_combobox.currentText()
@@ -352,21 +349,21 @@ class ConfigureDialog(QtWidgets.QDialog):
         self.original_auto_delay = self.auto_delay_checkbox.isChecked()
         self.original_delay = self.delay_spinbox.value()
 
-        reset_clear_layout = QtWidgets.QHBoxLayout()
+        reset_clear_layout = qt.QtWidgets.QHBoxLayout()
         reset_clear_layout.addWidget(self.clear_button)
         reset_clear_layout.addWidget(self.reset_button)
-        spacer = QtWidgets.QSpacerItem(1, 1, QtWidgets.QSizePolicy.Expanding, QtWidgets.QSizePolicy.Minimum)
+        spacer = qt.QtWidgets.QSpacerItem(1, 1, qt.QtWidgets.QSizePolicy.Expanding, qt.QtWidgets.QSizePolicy.Minimum)
         reset_clear_layout.addSpacerItem(spacer)
 
-        group_layout = QtWidgets.QHBoxLayout()
+        group_layout = qt.QtWidgets.QHBoxLayout()
         group_layout.addWidget(function_group)
         group_layout.addWidget(trigger_group)
 
-        apply_cancel_layout = QtWidgets.QHBoxLayout()
+        apply_cancel_layout = qt.QtWidgets.QHBoxLayout()
         apply_cancel_layout.addWidget(self.apply_button)
         apply_cancel_layout.addWidget(self.cancel_button)
 
-        layout = QtWidgets.QVBoxLayout()
+        layout = qt.QtWidgets.QVBoxLayout()
         layout.addLayout(reset_clear_layout)
         layout.addLayout(group_layout)
         layout.addLayout(apply_cancel_layout)
@@ -418,7 +415,7 @@ class ConfigureDialog(QtWidgets.QDialog):
 
     def save_settings(self):
         """Save the settings to the digital multimeter."""
-        application().setOverrideCursor(Qt.WaitCursor)
+        qt.application().setOverrideCursor(qt.Qt.WaitCursor)
         self.ask_user = False
 
         if self.parent.connection.connected_as_link:
@@ -440,7 +437,7 @@ class ConfigureDialog(QtWidgets.QDialog):
             self.restart_parent_timer()
 
         # close the QDialog
-        application().restoreOverrideCursor()
+        qt.application().restoreOverrideCursor()
         self.close()
 
     def on_reset(self):
@@ -470,6 +467,7 @@ class ConfigureDialog(QtWidgets.QDialog):
                     self.original_count != self.count_spinbox.value() or \
                     self.original_auto_delay is not self.auto_delay_checkbox.isChecked() or \
                     self.original_delay != self.delay_spinbox.value():
-                if prompt.yes_no('You have modified the settings.\n\nApply the changes?', default=False):
+                if qt.prompt.yes_no('You have modified the settings.\n\n'
+                                    'Apply the changes?', default=False):
                     self.save_settings()
         super(ConfigureDialog, self).closeEvent(event)
